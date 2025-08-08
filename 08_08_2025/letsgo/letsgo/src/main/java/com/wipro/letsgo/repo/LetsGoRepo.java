8341aa9201641e1f0fb2a88f56fe39f351893c6e
vaishnavi-datkar <118161986+vaishnavi-datkar@users.noreply.github.com>
2025-08-09 00:12:51 +0530

    Remove old 08_08_2025 folder from root

--- 08_08_2025/letsgo/letsgo/src/main/java/com/wipro/letsgo/repo/LetsGoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.letsgo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.letsgo.entity.LetsGo;
-
-public interface LetsGoRepo extends JpaRepository<LetsGo, Long> {
-
-}
