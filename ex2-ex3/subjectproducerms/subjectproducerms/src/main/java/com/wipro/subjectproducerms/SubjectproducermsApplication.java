28930121d7f89a1d8bf908c70d72410c01123199
vaishnavi-datkar <118161986+vaishnavi-datkar@users.noreply.github.com>
2025-08-11 01:39:39 +0530

    Initial commit

---
+++ ex2-ex3/subjectproducerms/subjectproducerms/src/main/java/com/wipro/subjectproducerms/SubjectproducermsApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.subjectproducerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectproducermsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectproducermsApplication.class, args);
+	}
+
+}
